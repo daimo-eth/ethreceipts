2fc2b1a8b78473faadc778b3b55a60de0cc67d1d
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-03-31 01:35:14 -0700

    host url

--- app/l/[blockNumber]/[logIndex]/page.tsx
+++ app/l/[blockNumber]/[logIndex]/page.tsx
@@ -3,7 +3,7 @@ import EventLogCard from "@/app/components/EventLogCard";
 
 /* Fetch log data from API */
 async function getLogData(blockNumber: string, logIndex: string) {
-    const res = await fetch(`${process.env.HOST_URL}/api/${blockNumber}/${logIndex}`, {cache: 'force-cache'});
+    const res = await fetch(`${process.env.HOST_API}/api/${blockNumber}/${logIndex}`, {cache: 'force-cache'});
     if (!res.ok) {
         throw new Error('Failed to fetch log');
     }
