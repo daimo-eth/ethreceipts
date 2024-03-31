2bf8245c939866941cacf201c2c81dc444380d9f
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-03-31 01:28:48 -0700

    rm eslint rule

--- app/utils/serialization.ts
+++ app/utils/serialization.ts
@@ -3,7 +3,6 @@
  * Reference: https://github.com/GoogleChromeLabs/jsbi/issues/30
  */
 
-// eslint-disable-next-line @typescript-eslint/no-redeclare
 interface BigInt {
     /** Convert to BigInt to string form in JSON.stringify */
     toJSON: () => string;
