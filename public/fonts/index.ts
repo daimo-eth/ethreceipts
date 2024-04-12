c51bc52472b15b7c350c72d5a60d62ba92c98d18
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-04-11 23:48:01 -0700

    update spelling

--- public/fonts/index.ts
+++ public/fonts/index.ts
@@ -2,19 +2,19 @@ import localFont from 'next/font/local';
 
 export const sfPro = localFont({
   src: [
-    { path: './sfPro/sf-pro-display_light.woff2', weight: '300', style: 'normal' },
-    { path: './sfPro/sf-pro-display_regular.woff2', weight: '400', style: 'normal' },
+    { path: './SfPro/sf-pro-display_light.woff2', weight: '300', style: 'normal' },
+    { path: './SfPro/sf-pro-display_regular.woff2', weight: '400', style: 'normal' },
     {
-      path: './sfPro/sf-pro-display_medium.woff2',
+      path: './SfPro/sf-pro-display_medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: './sfPro/sf-pro-display_semibold.woff2',
+      path: './SfPro/sf-pro-display_semibold.woff2',
       weight: '600',
       style: 'normal',
     },
-    { path: './sfPro/sf-pro-display_bold.woff2', weight: '700', style: 'normal' },
+    { path: './SfPro/sf-pro-display_bold.woff2', weight: '700', style: 'normal' },
   ],
 });
 
