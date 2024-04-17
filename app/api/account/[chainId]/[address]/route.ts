37ccac357f98f7459bb180ff76d2c3a65bc2e63a
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-04-17 12:47:07 -0700

    add profiles

--- app/api/account/[chainId]/[address]/route.ts
+++ app/api/account/[chainId]/[address]/route.ts
@@ -1,7 +1,27 @@
+import { getDaimoAccountHistory } from '@/app/utils/accountHistory/getDaimoAccountHistory';
 import { resolveAccountForAddress } from '@/app/utils/profiles';
 import { AddressProfile } from '@/app/utils/types';
 import { createViemClient } from '@/app/utils/viem/client';
-import { Hex } from 'viem';
+import { Address, Hex } from 'viem';
+
+// TransferLog type from Daimo API.
+// TODO: add type when Daimo API is updated.
+type TransferLog = {
+  type: string;
+  status: string;
+  timestamp: number;
+  from: string;
+  to: string;
+  amount: number;
+  blockNumber: number;
+  blockHash: string;
+  txHash: string;
+  logIndex: number;
+  nonceMetadata: string;
+  opHash: string;
+  requestStatus: string | null;
+  feeAmount: number;
+};
 
 /**
  * Handle GET requests to /api/account/[chainId]/[address]
@@ -17,10 +37,21 @@ export async function GET(
 ) {
   const publicClient = createViemClient(params.chainId);
 
-  const account: AddressProfile = await resolveAccountForAddress(
+  const accountHistory = await getDaimoAccountHistory(params.address as Address);
+  console.log(accountHistory);
+
+  const accountProfile: AddressProfile = await resolveAccountForAddress(
     params.address as Hex,
     publicClient,
   );
 
-  return Response.json(account);
+  // Filter out non-transfer logs.
+  const accountTransfers: TransferLog[] = accountHistory
+    ? accountHistory.transferLogs.filter((log: TransferLog) => log.type === 'transfer')
+    : [];
+
+  return Response.json({
+    accountProfile: accountProfile,
+    accountTransferHistory: accountTransfers,
+  });
 }
--- app/l/account/[chainId]/[address]/page.tsx
+++ app/l/account/[chainId]/[address]/page.tsx
@@ -1,11 +1,16 @@
 import AddressBubble from '@/app/components/AddressBubble';
-import Footer from '@/app/components/Footer';
 import { Header } from '@/app/components/typography';
 import { apiUrl } from '@/app/env';
 import { AddressProfile } from '@/app/utils/types';
 
+// Acount profile and transfer history.
+type Account = {
+  accountProfile: AddressProfile;
+  accountTransferHistory: any[];
+};
+
 /** Retrieve address profile given chainId and address. */
-async function getAddressProfile(chainId: string, address: string): Promise<AddressProfile | null> {
+async function getAddressProfile(chainId: string, address: string): Promise<Account | null> {
   const res = await fetch(`${apiUrl}/api/account/${chainId}/${address}`);
   if (!res.ok) {
     console.error('Failed to fetch address profile');
@@ -24,10 +29,7 @@ async function getAddressProfile(chainId: string, address: string): Promise<Addr
  * @returns {React.ReactElement} An account page component.
  */
 export default async function Page({ params }: { params: { chainId: string; address: string } }) {
-  const addressProfile: AddressProfile | null = await getAddressProfile(
-    params.chainId,
-    params.address,
-  );
+  const addressProfile: Account | null = await getAddressProfile(params.chainId, params.address);
   if (!addressProfile) return null;
 
   return (
@@ -35,7 +37,14 @@ export default async function Page({ params }: { params: { chainId: string; addr
       <div className='flex sm:pt-20 pt-12 sm:pb-14 pb-8'>
         <Header>ETH RECEIPT</Header>
       </div>
-      <AddressBubble addressProfile={addressProfile} />
+      <div
+        className='rounded-[24px] flex flex-col w-full m-auto
+      border-[0px] bg-gradient-to-b from-[#F3F3F3] to-[#D6D6D6] p-[1px] drop-shadow-3xl min-w-[600px]'
+      >
+        <div className='flex flex-col bg-white rounded-[23px] px-10 py-6'>
+          <AddressBubble addressProfile={addressProfile.accountProfile} />
+        </div>
+      </div>
     </div>
   );
 }
