a6e3e192d4b4fa0a5cc7b04b8c6c1357e983fada
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-04-17 10:44:50 -0700

    move footer

--- app/l/[chainId]/[blockNumber]/[logIndex]/page.tsx
+++ app/l/[chainId]/[blockNumber]/[logIndex]/page.tsx
@@ -58,7 +58,6 @@ export default async function Page({
       ) : (
         <LogNotFound />
       )}
-      <Footer />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { sfPro } from '@/public/fonts';
 import './globals.css';
+import Footer from './components/Footer';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={sfPro.className}>{children}</body>
+      <body className={sfPro.className}>
+        {children} <Footer />
+      </body>
     </html>
   );
 }
