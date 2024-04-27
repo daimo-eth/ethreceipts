08f46305c8c908692ff4bbc3289dfc51f07f0cda
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-04-27 07:43:01 -0700

    change main text

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 /* TODO: MAKE THIS PRETTY */
 export default function Home() {
-  return 'go to /l/[blockNumber]/[logIndex] to see the log page for a log.';
+  return (
+    <div>
+      <div className='flex flex-col items-center justify-center w-full h-full'>
+        <div>
+          The goal of Eth Receipts is to show ERC-20 transfers in a clean and extensible way. No
+          information overload, no noise. Just a simple receipt.
+        </div>
+        <div>Go to /l/[blockNumber]/[logIndex] to see the log page for a log.</div>
+      </div>
+    </div>
+  );
 }
