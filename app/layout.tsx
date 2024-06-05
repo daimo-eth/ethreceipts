92417a39ae37c1769da5afac658193e6bbed8dac
Kaylee George <kaylee@daimo.com>
2024-06-04 18:09:00 -0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,27 @@ import { sfPro } from '@/public/fonts';
 import './globals.css';
 import Footer from './components/shared/Footer';
 import { Header } from './components/shared/Header';
+import { Metadata } from 'next';
+import { getAbsoluteUrl } from './utils/getAbsoluteUrl';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(getAbsoluteUrl('/')),
+  title: 'Eth Receipts',
+  description: 'Receipts for Ethereum Transactions',
+  icons: {
+    icon: '/receipt-logo.png',
+  },
+  openGraph: {
+    images: [
+      {
+        url: '/eth-receipts-header.png',
+        width: 138,
+        height: 71,
+        alt: 'Eth Receipts',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
